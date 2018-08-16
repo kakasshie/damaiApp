00f92bbe6463e55375f25a5210ce61e33a2a9e23
kakasshie <kakasshie@163.com>
2018-08-16 10:35:42 +0800

    API改善

---
+++ src/components/HeadTop/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="headTop">
+        <div class="headTop_cont floatLeft">
+            <span>Hi,欢迎[<span>{{ username }}</span>，<span>退出</span>]登录</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "HeadTop",
+        props:["username"]
+    }
+
+</script>
+
+<style scoped>
+    .headTop{
+        width: 1000px;
+        margin:0 auto;
+        padding: 15px;
+        overflow: hidden;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from "@/components/Home"
+import Login from "@/pages/Login"
+import Admin from '@/pages/Admin'
+import OrderMenu from '@/components/Order/OrderMenu'
+import MyMsg from "@/components/Order/MyMsg"
 
 Vue.use(Router)
 
@@ -10,6 +15,24 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path: '/admin',
+      name: "Admin",
+      component: Admin,
+      children:[{
+        path:"",
+        name:"OrderMenu",
+        component:OrderMenu
+      },{
+        path: 'ordermenu',
+        name: 'OrderMenu',
+        component: OrderMenu
+      },{
+        path:"mymsg",
+        name: 'MyMsg',
+        component: MyMsg
+      }]
     }
   ]
 })
+
